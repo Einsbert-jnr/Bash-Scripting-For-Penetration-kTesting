bdca5d368aa7901964ed9bd28b7b9f61b55c66c2
Clement Heanampong <einsbert.jnr@gmai.com>
2024-07-06 15:29:32 +0000

    Task 2 script and output

---
+++ Assignments/Task2/task-2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is a script that checks if a directory exist and if not, create it and create three files in that directory
+arg1=$1
+arg2=$2
+arg3=$3
+
+for i in $arg1 $arg2 $arg3
+do
+	if [ -d $i ];
+	then
+		echo "Directory $i already exit!!"
+	else
+		mkdir $i &&  $(touch $i/file1 $i/file2 $i/file3)
+	fi
+done
